dfec935563d18f4ef0c9c5989e431d2950c09f5a
Izabel Vidal <izabelvidal99@gmail.com>
2020-09-09 14:54:53 -0300

    layout quase pronto

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <link href="/styles/styles.css" rel="stylesheet">
+    </head>
+    <body>
+        <!--navegação-->
+        <nav>
+            <button>
+                Cadastro
+            </button>
+            <button>
+                Login
+            </button>
+        </nav>
+
+
+        <div class="main">
+            <!--<div class="imagem">
+                aqui sera uma imagem
+            </div>
+
+            <article>
+                <h1>Lorem Ipsum</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Aenean imperdiet urna tempus tristique faucibus. Vestibulum et nibh ullamcorper, placerat enim sed, malesuada purus.
+                    Proin nec leo erat. Curabitur bibendum ullamcorper pharetra. In non pharetra tortor. Vivamus ut sagittis nisl.
+                    Vestibulum ultricies sapien dignissim facilisis dapibus.
+                    Vivamus auctor fringilla felis, eget volutpat leo commodo ac.
+                </p>
+            </article>
+        </div>-->
+        <footer></footer>
+    </body>
+</html>
