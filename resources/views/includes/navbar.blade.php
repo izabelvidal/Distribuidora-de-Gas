a861ec6272bd3e5bdaebea3b82753aac98751f8e
Izabel Vidal <izabelvidal99@gmail.com>
2020-09-20 11:54:55 -0300

    flexbox

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,8 +1,15 @@
 <nav>
-    <button>
-        Cadastro
-    </button>
-    <button>
-        Login
-    </button>
+    <div class="logo">
+        aqui tem uma logo
+    </div>
+
+    <div class="botoes">
+        <button>
+            Cadastro
+        </button>
+        <button>
+            Login
+        </button>
+    </div>
+    
 </nav>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,9 @@
 
         <title>Laravel</title>
 
-        <link href="/styles/styles.css" rel="stylesheet">
+        <link href="/styles/app.css" rel="stylesheet">
+        <link href="/styles/navbar.css" rel="stylesheet">
+        <link href="/styles/footer.css" rel="stylesheet">
     </head>
     <body>
         @include("includes.navbar")
