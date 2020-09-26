f55142d80cb2e11bfa5297aef82cf65712e13138
Letícia Araújo <leticiaaraujo6392@gmail.com>
2020-09-26 16:54:31 -0300

    Corrigindo venda

--- app/Models/Venda.php
+++ app/Models/Venda.php
@@ -8,20 +8,20 @@ class Venda extends Model
 {
 
     protected $fillable = [
-        'Produto', 'Cliente', 'preco', 'data', 'quantidade_de_pedidos'
+        'preco', 'data'
     ];
 
     public static $rules = [
-        'Prioduto' => 'required',
-        'Cliente' => 'required',
+        'Item' => 'required',
+        'cliente_id' => 'required',
         'preco' => 'required|numeric|min:30',
         'data' => 'required|data',
         'quantidade_de_pedidos' => 'required|numeric|min:1',
     ];
 
     public static $messages = [
-        'Produto.*' => 'O campo Produto é obrigatório',
-        'Cliente.*' => 'O campo Cliente',
+        'Item.*' => 'O campo Produto é obrigatório',
+        'cliente_id.*' => 'O campo Cliente',
         'preco.*' => 'O campo preço é obrigatório e deve ser de no mínimo R$30,00',
         'data.*' => 'O campo data é obrigatório',
         'quantidade_de_pedidos.*' => 'O campo quantidade de pedidos é obrigatório e deve ser de no mínimo 1'
