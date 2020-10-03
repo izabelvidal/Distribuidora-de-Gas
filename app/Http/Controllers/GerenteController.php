8b5a7b9a223353f708d99bcdfb852debc057b28e
Letícia Araújo <leticiaaraujo6392@gmail.com>
2020-10-03 19:21:26 -0300

    Criando funçao de cadastrar produto

--- app/Http/Controllers/GerenteController.php
+++ app/Http/Controllers/GerenteController.php
@@ -88,4 +88,8 @@ class GerenteController extends Controller
         $gerente->pessoa()->delete();
         return redirect()->action([GerenteController::class, 'index']);
     }
+
+    public function cadastroProduto(Gerente $gerente){
+
+    }
 }
