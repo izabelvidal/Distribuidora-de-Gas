6d34f61b6b6e08420388eecb3cfeaee1f20a571e
Letícia Araújo <leticiaaraujo6392@gmail.com>
2020-10-08 17:41:41 -0300

    corrigindo views de novo

--- resources/views/produtos/show.blade.php
+++ resources/views/produtos/show.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2> {{ $project->name }}</h2>
+                <h2> {{ $produto->nome }}</h2>
             </div>
             <div class="pull-right">
                 <a class="btn btn-primary" href="{{ route('projects.index') }}" title="Go back"> <i
@@ -17,8 +17,8 @@
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
-                <strong>Name:</strong>
-                {{ $project->name }}
+                <strong>Marca:</strong>
+                {{ $produto->name }}
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
