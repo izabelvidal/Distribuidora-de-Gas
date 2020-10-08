9512168359d9c178fc2bb6427eccac47db16e5cf
Izabel Vidal <izabelvidal99@gmail.com>
2020-10-08 17:27:19 -0300

    erro do controller

--- resources/views/gerentes/index.blade.php
+++ resources/views/gerentes/index.blade.php
@@ -1,22 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-    <table >
-            <tr >
-                <td style="border-style: solid;
-    border-width: 5px;border-color: blue;">{{ $gerente->pessoa->nome }}</td>
-                <td style="border-style: solid;
-    border-width: 5px;border-color: blue;">{{ $gerente->pessoa->nascimento }}</td>
-                <td style="border-style: solid;
-    border-width: 5px;border-color: blue;">{{ $gerente->pessoa->email }}</td>
-                <td style="border-style: solid;
-    border-width: 5px;border-color: blue;">{{ $gerente->pessoa->senha }}</td>
-                <td  style="border-style: solid;
-    border-width: 5px;border-color: blue;">{{ $gerente->pessoa->CPF }}</td>
-                <td style="border-style: solid;
-    border-width: 5px;border-color: blue;">{{ $gerente->telefone }}</td>
-                <td style="border-style: solid;
-    border-width: 5px;border-color: blue;">{{ $gerente->tipo }}</td>
+    <table class="table">
+        <thead>
+        <tr>
+            <th>Nome</th>
+            <th>CPF</th>
+            <th>Email</th>
+            <th>Opções</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($gerentes as $gerente)
+            <tr>
+                <td>{{ $gerente->pessoa->nome }}</td>
+                <td>{{ $gerente->pessoa->CPF }}</td>
+                <td>{{ $gerente->pessoa->email }}</td>
+                <td>
+                    <a href="{{route('gerentes.edit', [$gerente])}}">editar</a>
+                    <a href="{{route('gerentes.show', [$gerente])}}">visualizar</a>
+                </td>
             </tr>
+        @endforeach
+        </tbody>
     </table>
 @endsection
