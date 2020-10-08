a569dc42f080d5bda653f858adccc10caa6ac868
José Fernando Mendes da Costa <fmdinho3@gmail.com>
2020-10-08 13:58:25 -0300

    adiciona/atualiza views de criar, visualizar e editar o cliente

--- resources/views/clientes/edit.blade.php
+++ resources/views/clientes/edit.blade.php
@@ -5,31 +5,23 @@
         @csrf
         <div class="form-group">
             <label for="nome">nome:</label>
-            <input class="form-control" type="text" name="nome" id="nome" value="{{ old('nome') }}">
+            <input class="form-control" type="text" name="nome" id="nome" value="{{ $cliente->pessoa->nome }}">
         </div>
         <div class="form-group">
             <label for="CPF">CPF:</label>
-            <input class="form-control" type="text" name="CPF" id="CPF" value="{{ old('CPF') }}">
+            <input class="form-control" type="text" name="CPF" id="CPF" value="{{ $cliente->pessoa->CPF }}">
         </div>
         <div class="form-group">
             <label for="nascimento">nascimento:</label>
-            <input class="form-control" type="date" name="nascimento" id="nascimento" value="{{ old('nascimento') }}">
+            <input class="form-control" type="date" name="nascimento" id="nascimento" value="{{ $cliente->pessoa->nascimento }}">
         </div>
         <div class="form-group">
             <label for="email">email:</label>
-            <input class="form-control" type="email" name="email" id="email" value="{{ old('email') }}">
-        </div>
-        <div class="form-group">
-            <label for="senha">senha:</label>
-            <input class="form-control" type="password" name="senha" id="senha">
-        </div>
-        <div class="form-group">
-            <label for="senha_confirmation">senha confirmation:</label>
-            <input class="form-control" type="password" name="senha_confirmation" id="senha_confirmation">
+            <input class="form-control" type="email" name="email" id="email" value="{{ $cliente->pessoa->email }}">
         </div>
         <div class="form-group">
             <label for="telefone">telefone:</label>
-            <input class="form-control" type="text" name="telefone" id="telefone" value="{{ old('telefone') }}">
+            <input class="form-control" type="text" name="telefone" id="telefone" value="{{ $cliente->telefone }}">
         </div>
         <div class="form-group">
             <label for="tipo">tipo</label>
@@ -42,23 +34,23 @@
         </div>
         <div class="form-group">
             <label for="rua">rua:</label>
-            <input class="form-control" type="text" name="rua" id="rua" value="{{ old('rua') }}">
+            <input class="form-control" type="text" name="rua" id="rua" value="{{ $cliente->pessoa->endereco->rua }}">
         </div>
         <div class="form-group">
             <label for="bairro">bairro:</label>
-            <input class="form-control" type="text" name="bairro" id="bairro" value="{{ old('bairro') }}">
+            <input class="form-control" type="text" name="bairro" id="bairro" value="{{ $cliente->pessoa->endereco->bairro }}">
         </div>
         <div class="form-group">
             <label for="CEP">CEP:</label>
-            <input class="form-control" type="text" name="CEP" id="CEP" value="{{ old('CEP') }}">
+            <input class="form-control" type="text" name="CEP" id="CEP" value="{{ $cliente->pessoa->endereco->CEP }}">
         </div>
         <div class="form-group">
             <label for="numero">numero:</label>
-            <input class="form-control" type="text" name="numero" id="numero" value="{{ old('numero') }}">
+            <input class="form-control" type="text" name="numero" id="numero" value="{{ $cliente->pessoa->endereco->numero }}">
         </div>
         <div class="form-group">
             <label for="cidade">cidade:</label>
-            <input class="form-control" type="text" name="cidade" id="cidade" value="{{ old('cidade') }}">
+            <input class="form-control" type="text" name="cidade" id="cidade" value="{{ $cliente->pessoa->endereco->cidade }}">
         </div>
         <input class="btn btn-primary" type="submit" value="cadastrar">
     </form>
