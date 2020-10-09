2a755391e479243ee20c29d6aa9dcaca9fc15636
Izabel Vidal <izabelvidal99@gmail.com>
2020-10-09 15:29:40 -0300

    erro no edit

--- resources/views/gerentes/edit.blade.php
+++ resources/views/gerentes/edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <form action="{{route('gerentes.update', $gerente)}}" method="post">
+    <form action="{{route('gerentes.update', $gerente)}}" method="POST">
         @csrf
         <div class="form-group">
             <label>Nome</label>
@@ -28,7 +28,7 @@
         <div class="form-row mt-3">
             <div class="col-md-6">
                 <label>Telefone</label>
-                <input type="number" class="form-control" placeholder="Telefone" name="telefone" value="{{ $gerente->telefone' }}">
+                <input type="number" class="form-control" placeholder="Telefone" name="telefone" value="{{ $gerente->pessoa->telefone' }}">
             </div>
         </div>
         <div class="form-row mt-3">
@@ -51,42 +51,9 @@
                 <label>Cidade</label>
                 <input type="text" class="form-control" id="cidade" placeholder="Cidade" name="cidade" value="{{ $gerente->pessoa->endereco->cidade }}">
             </div>
-            <div class="form-group col-md-4">
-                <label>Estado</label>
-                <select id="estado" name="estado" class="form-control">
-                <option selected disabled value="">Selecionar Estado</option>
-                    <option value="AC">Acre</option>
-                    <option value="AL">Alagoas</option>
-                    <option value="AP">Amapá</option>
-                    <option value="AM">Amazonas</option>
-                    <option value="BA">Bahia</option>
-                    <option value="CE">Ceará</option>
-                    <option value="DF">Distrito Federal</option>
-                    <option value="ES">Espírito Santo</option>
-                    <option value="GO">Goiás</option>
-                    <option value="MA">Maranhão</option>
-                    <option value="MT">Mato Grosso</option>
-                    <option value="MS">Mato Grosso do Sul</option>
-                    <option value="MG">Minas Gerais</option>
-                    <option value="PA">Pará</option>
-                    <option value="PB">Paraíba</option>
-                    <option value="PR">Paraná</option>
-                    <option value="PE">Pernambuco</option>
-                    <option value="PI">Piauí</option>
-                    <option value="RJ">Rio de Janeiro</option>
-                    <option value="RN">Rio Grande do Norte</option>
-                    <option value="RS">Rio Grande do Sul</option>
-                    <option value="RO">Rondônia</option>
-                    <option value="RR">Roraima</option>
-                    <option value="SC">Santa Catarina</option>
-                    <option value="SP">São Paulo</option>
-                    <option value="SE">Sergipe</option>
-                    <option value="TO">Tocantins</option>
-                </select>
-            </div>
             <div class="form-group col-md-2">
                 <label>CEP</label>
-                <input type="text" class="form-control" name="CEP" id="CEP" placeholder="Ex: 55158-400" value="{{ old('CEP') }}">
+                <input type="text" class="form-control" name="CEP" id="CEP" placeholder="Ex: 55158-400" value="$gerente->pessoa->endereco->\cep">
             </div>
         </div>
     
