c1279fa522a2814a88d8ed14e2da05713d71e326
José Fernando Mendes da Costa <fmdinho3@gmail.com>
2020-10-17 10:07:00 -0300

    mostra o valor total, forms para atualizar e remover o item

--- resources/views/carrinho.blade.php
+++ resources/views/carrinho.blade.php
@@ -9,10 +9,35 @@
                     <div class="card-body">
                         <h1>Itens</h1>
                         <ul>
-                            @foreach (Session::get('itens') as $k => $item )
-                                <li>{{ $item['produto'] }} - {{$item['quantidade']}} - {{$item['preco']}} <a
-                                        href="{{ route('remover', ['produto_id' => $k]) }}">Remover</a></li>
-                            @endforeach
+                            @if(session()->has('itens'))
+                                @foreach (session('itens') as $key => $value )
+                                    <li>
+                                        <form action="{{ route('carrinho.update', ['produto_id' => $key]) }}"
+                                              id="update-{{$key}}" method="POST" style="display: inline">
+                                            @csrf
+                                            @method('PUT')
+                                            <select name="quantidade" id="quantidade"
+                                                    onchange="document.getElementById('update-{{$key}}').submit()"
+                                                    style="display: inline">
+                                                @for($i = 1; $i <= $value['produto']->quantidade_em_estoque; $i++)
+                                                    <option value="{{$i}}"
+                                                        {{($i == $value['quantidade']) ? 'selected' : ''}}>{{$i}}
+                                                    </option>
+                                                @endfor
+                                            </select>
+                                        </form>
+                                        {{ $value['produto']->nome }}
+                                        ; preço unitário: {{$value['produto']->preco}}; subtotal: {{$value['subtotal']}}
+                                        <form action="{{ route('carrinho.destroy', ['produto_id' => $key]) }}"
+                                              id="delete-{{$key}}" method="POST" style="display: inline">
+                                            @csrf
+                                            @method('DELETE')
+                                            <a href="#" onclick="document.getElementById('delete-{{$key}}').submit()">Remover</a>
+                                        </form>
+                                    </li>
+                                @endforeach
+                                total: {{session()->get('total')}}
+                            @endif
                         </ul>
                     </div>
                 </div>
