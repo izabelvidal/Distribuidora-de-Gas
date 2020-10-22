b289aa7e656ae7ac4f50e423cd488dd2736f3cc9
José Fernando Mendes da Costa <fmdinho3@gmail.com>
2020-10-22 19:06:49 -0300

    registra as policies

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,18 @@
 
 namespace App\Providers;
 
+use App\Models\Cliente;
+use App\Models\Funcionario;
+use App\Models\Gerente;
+use App\Models\Produto;
 use App\Models\Team;
+use App\Models\Venda;
+use App\Policies\ClientePolicy;
+use App\Policies\FuncionarioPolicy;
+use App\Policies\GerentePolicy;
+use App\Policies\ProdutoPolicy;
 use App\Policies\TeamPolicy;
+use App\Policies\VendaPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -15,6 +25,11 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         Team::class => TeamPolicy::class,
+        Venda::class => VendaPolicy::class,
+        Funcionario::class => FuncionarioPolicy::class,
+        Cliente::class => ClientePolicy::class,
+        Produto::class => ProdutoPolicy::class,
+        Gerente::class => GerentePolicy::class,
     ];
 
     /**
