6a5f477b21f5583e3142b8d4553958fa33fb7a44
José Fernando Mendes da Costa <fmdinho3@gmail.com>
2020-10-22 20:15:34 -0300

    utiliza a cor verde para mostrar as mensagens

--- resources/views/includes/messages.blade.php
+++ resources/views/includes/messages.blade.php
@@ -8,7 +8,7 @@
     </div>
 @endif
 @if (session('message'))
-    <div class="alert alert-danger">
+    <div class="alert alert-success">
         <ul>
                 <li>{{ session('message') }}</li>
         </ul>
