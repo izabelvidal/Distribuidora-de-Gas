04e1db51fb53ff1da0834799e4365ee3e37a3254
LeticiaAraujo26 <leticiaaraujo6392@gmail.com>
2020-10-30 12:16:33 -0300

    testes

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -2,11 +2,7 @@
 
 namespace Tests\Browser;
 
-use App\Models\Gerente;
-use App\Models\Pessoa;
 use App\Models\User;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
 
@@ -19,18 +15,14 @@ class LoginTest extends DuskTestCase
      */
     public function testExample()
     {
-        $user = User::find(1)->first(); 
+        $user = User::find(1)->first();
         $this->browse(function ($browser) use ($user) {
             $browser
                 ->visit('/login')
                 ->type('email', $user->email)
                 ->type('password', 'password')
-                ->pause(2000)
-                ->screenshot('login')
                 ->press('submit')
-                ->assertPathIs('/home')
-                ->pause(2000)
-                ->screenshot('home');
+                ->assertPathIs('/home');
         });
     }
 }
--- tests/Browser/ProdutoTest.php
+++ tests/Browser/ProdutoTest.php
@@ -2,11 +2,7 @@
 
 namespace Tests\Browser;
 
-use App\Models\Produto;
-use App\Models\Gerente;
-use App\Models\Pessoa;
 use App\Models\User;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
@@ -19,7 +15,7 @@ class ProdutoTest extends DuskTestCase
      */
     public function testExample()
     {
-        $gerente = User::where('tipo', 'gerente')->first();       
+        $gerente = User::where('tipo', 'gerente')->first();
         $this->browse(function (Browser $browser) use ($gerente)
         {
             $browser
@@ -28,12 +24,11 @@ class ProdutoTest extends DuskTestCase
                 ->type('nome', 'Gás de cozinha')
                 ->type('marca', 'Fugaz')
                 ->type('quantidade_em_estoque', '40')
-                ->type('peso', '5')
+                ->type('peso', '15')
                 ->type('preco', '60.00')
                 ->type('preco_revenda', '50.00')
-                ->pause(2000)
-                ->assertSee('Gás de cozinha');
+                ->press('cadastrar')
+                ->assertSee('Fugaz');
         });
-        
     }
 }
